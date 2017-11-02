8f0fc44177bfc7932ca26f475b4aacf11bf1134b
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2017-11-02 22:55:49 +0000

    use as_typed_slice

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,28 @@
 extern crate glium;
 
-use glium::vertex::{Vertex, VertexBuffer, IntoVerticesSource, VerticesSource};
+use glium::vertex::{Vertex, VertexBuffer, VertexBufferAny, IntoVerticesSource, VerticesSource};
+use glium::buffer::{BufferSlice, BufferMutSlice};
 use std::any::TypeId;
+use std::ops::Deref;
 use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult};
-use std::{mem, ptr};
 
 /// A type-erased `VertexBuffer` which can be safely borrowed as the original `VertexBuffer<T>`
 /// using a dynamic type check.
 pub struct TypedVertexBufferAny {
     type_id: TypeId,
-    buffer: VertexBuffer<u8>,
+    buffer: VertexBufferAny,
 }
 
 impl TypedVertexBufferAny {
     /// Borrows buffer immutably as typed, checking the type at runtime.
     ///
     /// Returns an error if the type is not the one given on construction.
-    pub fn downcast<T: Vertex + 'static>(&self) -> Result<&VertexBuffer<T>, IncorrectTypeError> {
+    pub fn as_typed_slice<T: Vertex + Send + 'static>(
+        &self,
+    ) -> Result<BufferSlice<[T]>, IncorrectTypeError> {
         if self.type_id == TypeId::of::<T>() {
-            Ok(unsafe {
-                &*(&self.buffer as *const _ as *const VertexBuffer<T>)
-            })
+            Ok(unsafe { self.buffer.deref().as_typed_slice() })
         } else {
             Err(IncorrectTypeError)
         }
@@ -30,13 +31,11 @@ impl TypedVertexBufferAny {
     /// Borrows the buffer mutably as typed, checking the type at runtime.
     ///
     /// Returns an error if the type is not the one given on construction.
-    pub fn downcast_mut<T: Vertex + 'static>(
+    pub fn as_typed_slice_mut<T: Vertex + Send + 'static>(
         &mut self,
-    ) -> Result<&mut VertexBuffer<T>, IncorrectTypeError> {
+    ) -> Result<BufferMutSlice<[T]>, IncorrectTypeError> {
         if self.type_id == TypeId::of::<T>() {
-            Ok(unsafe {
-                &mut *(&mut self.buffer as *mut _ as *mut VertexBuffer<T>)
-            })
+            Ok(unsafe { self.buffer.as_typed_slice_mut() })
         } else {
             Err(IncorrectTypeError)
         }
@@ -45,24 +44,13 @@ impl TypedVertexBufferAny {
 
 impl<T> From<VertexBuffer<T>> for TypedVertexBufferAny
 where
-    T: Vertex + 'static,
+    T: Vertex + Send + 'static,
 {
     fn from(buffer: VertexBuffer<T>) -> TypedVertexBufferAny {
-        // These should statically be true, since `T` is stored by glium only via a zero-sized
-        // `PhantomData`.
-        assert_eq!(mem::size_of_val(&buffer), mem::size_of::<VertexBuffer<T>>());
-        assert_eq!(
-            mem::align_of_val(&buffer),
-            mem::align_of::<VertexBuffer<T>>()
-        );
-
-        let type_erased = TypedVertexBufferAny {
+        TypedVertexBufferAny {
             type_id: TypeId::of::<T>(),
-            buffer: unsafe { ptr::read(&buffer as *const _ as *const VertexBuffer<u8>) },
-        };
-        mem::forget(buffer);
-
-        type_erased
+            buffer: buffer.into(),
+        }
     }
 }
 
